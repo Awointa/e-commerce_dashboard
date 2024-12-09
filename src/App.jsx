f015f870b25bb4588e19036b92531256d3a62117
Awointa <akosawointa@gmail.com>
2024-12-09 12:18:17 +0100

    chore(App)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-	return <h1 className="text-green-700">Bonjour</h1>;
+	return (
+		<div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto_auto_auto]">
+			<Sidebar />
+		</div>
+	);
 }
 
 export default App;

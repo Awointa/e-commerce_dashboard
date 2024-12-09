a420ff9bc1371e2431ddba7d92087508077679c5
Awointa <akosawointa@gmail.com>
2024-12-09 12:29:57 +0100

    feat(Sidebar):the former contetnt in the sidebar was supposed to be in the ProductCard component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,5 @@
 function Sidebar() {
-	return (
-		<div className=" bg-purple-700 p-4 flex flex-col items-star w-[300px] text-white rounded-lg gap-2">
-			<div className="flex gap-2">
-				{" "}
-				<h3>Product viewed</h3>
-				<span className="text-gray-700">2.9%</span>
-			</div>
-			<p className="font-bold">411.9K</p>
-			<div className="flex justify-between">
-				<p>vs. last month</p>
-				<p>3.13.2k</p>
-			</div>
-		</div>
-	);
+	return <div></div>;
 }
 
 export default Sidebar;
